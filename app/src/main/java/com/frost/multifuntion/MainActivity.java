65542ffc74893a2c670d1e65686b89db226858d9
Juan C <juan.cisilino@gmail.com>
2019-12-09 13:51:31 -0300

    Creacion calculadora e icono conversor

--- app/src/main/java/com/frost/multifuntion/MainActivity.java
+++ app/src/main/java/com/frost/multifuntion/MainActivity.java
@@ -2,13 +2,26 @@ package com.frost.multifuntion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button calculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calculator = findViewById(R.id.MainActivity_Button_Calculator);
+
+        calculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ActivityCalculator.class));
+            }
+        });
     }
 }
